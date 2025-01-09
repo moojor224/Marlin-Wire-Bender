74bdaeb90d1a3a95cbc578010b757852236af642
moojor224 <moojor224@gmail.com>
2025-01-08 19:23:39 -0600

    Fix error with servo config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3674,7 +3674,7 @@
 #define SERVO_DELAY { 300 }
 
 // Only power servos during movement, otherwise leave off to prevent jitter
-#define DEACTIVATE_SERVOS_AFTER_MOVE
+// #define DEACTIVATE_SERVOS_AFTER_MOVE
 
 // Edit servo angles with M281 and save to EEPROM with M500
 //#define EDITABLE_SERVO_ANGLES
